b10beb206195e52b6f69163091409c69493840b7
j8-blackburn <jamie2.blackburn@live.uwe.ac.uk>
2026-04-09 16:25:50 +0100

    Update main.cpp
     Main code for Main task 1.2, this program allows for three LEDs to be working at different
        blink rates running at the same time instead of individually.

--- Week 1/task 2/main.cpp
+++ Week 1/task 2/main.cpp
@@ -1,1 +1,56 @@
+/* 
+    Main code for Main task 1.2, this program allows for three LEDs to be working at different
+    blink rates running at the same time instead of individually.
+ */
 
+#include "mbed.h"
+
+// Blinking rate in milliseconds
+#define BLINK_RATE1     1000ms
+#define BLINK_RATE2     500ms
+#define BLINK_RATE3     2000ms
+
+
+// Output's have been moved to be used in all functions
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+
+//Functions for all LEDs to allow to run all at once
+void LED_1(){
+    while (true){
+    led1 = !led1;
+        ThisThread::sleep_for(BLINK_RATE1);
+    }
+}
+//Functions for all LEDs to allow to run all at once
+void LED_2(){
+    while (true){
+    led2 = !led2;
+        ThisThread::sleep_for(BLINK_RATE2);
+    }
+}
+//Functions for all LEDs to allow to run all at once
+void LED_3(){
+    while (true){
+    led3 = !led3;
+        ThisThread::sleep_for(BLINK_RATE3);
+    }
+}
+
+int main()
+{
+    // Create class of thread to allow all threads to be played at once instead of individually
+    Thread thread1;
+    Thread thread2;
+    Thread thread3;
+    // Starts all threads to be played at the same time
+    thread1.start(LED_1);
+    thread2.start(LED_2);
+    thread3.start(LED_3);
+
+    while (true) {
+    //This while loops keeps main running
+    ThisThread::sleep_for(1000ms);
+    }
+}
